bb6f8c20920f9006ce598a1fc2453b574decfcfe
DylanPJackson <frprdjackson@gmail.com>
2024-01-31 11:10:46 -0800

    Made tests more scalable, started sift_down() and pop()

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -25,8 +25,8 @@ class Heap:
     ( BASE )
     Peek : Find root
     Push : Add new item (sift up)
-    Pop : Return root value after removing from heap
-    Delete Root : Remove root node
+    Pop : Return root value after removing from heap (sift down)
+    Delete Root : Remove root node (sift down)
     Replace : Pop root and push new key (sift down)
 
     ( INTERNAL )
@@ -37,16 +37,16 @@ class Heap:
     Heapify : Create heap out of given array of elements
     Merge : Merge two heaps into one
     """
-    def __init__(self, kind: HeapType):
+    def __init__(self):
         self.is_empty = True
-        self.kind = kind
+        self.kind = None
         self._heap_arr = []
 
     @property
     def size(self):
         return len(self._heap_arr)
 
-    def reset(self):
+    def empty(self):
         self.is_empty = True
         self._heap_arr = []
 
@@ -70,27 +70,6 @@ class Heap:
         else:
             raise Exception("HeapType {kind} not supported".format(kind=self.kind))
 
-    def peek(self):
-        """
-        Return root of heap
-        :return: int, root
-        """
-        if self.is_empty:
-            raise Exception("Heap is empty, can not peek")
-        else:
-            return self._heap_arr[0]
-
-    def push(self, node: int):
-        """
-        Add node to heap. Utilizes _sift_up to satisfy heap property
-        :param node: int, new node
-        :return: None
-        """
-        self._heap_arr.append(node)
-        if self.is_empty:
-            self.is_empty = False
-        self._sift_up()
-
     def _sift_up(self):
         """
         Satisfy heap property by sifting up the heap
@@ -113,3 +92,69 @@ class Heap:
                     self._heap_arr[par_ind] = child_node 
                     self._heap_arr[child_ind] = par_node
                     child_ind = par_ind
+
+    def _sift_down(self):
+        """
+        Satisfy heap property by sifting down the heap
+
+        Repeatedly check, until satisfied or at leaf, that given index i, nodes at 2i+1 and 2i+2 satisfy heap property
+        :return: None
+        """
+        if self.is_empty:
+            raise Exception("Heap is empty, can not sift down")
+        else:
+            par_ind = 0
+            while par_ind < (self.size - 1):
+                child_1_ind = (2 * par_ind) + 1
+                child_2_ind = (2 * par_ind) + 2
+                par_node = self._heap_arr[par_ind]
+                if child_1_ind < self.size - 1:
+                    child_1_node = self._heap_arr[child_1_ind]
+                else:
+                    child_1_node = 0
+
+                if child_2_ind < self.size - 1:
+                    child_2_node = self._heap_arr[child_2_ind]
+                else:
+                    child_2_node = 0
+
+    def peek(self):
+        """
+        Return root of heap
+        :return: int, root
+        """
+        if self.is_empty:
+            raise Exception("Heap is empty, can not peek")
+        else:
+            return self._heap_arr[0]
+
+    def push(self, node: int):
+        """
+        Add node to heap. Utilizes _sift_up to satisfy heap property
+        :param node: int, new node
+        :return: None
+        """
+        self._heap_arr.append(node)
+        if self.is_empty:
+            self.is_empty = False
+        self._sift_up()
+
+    def pop(self):
+        """
+        Remove root node and return it.
+
+        Replaces root with most recently added node. Sift down with new root node.
+        :return: int, root node
+        """
+        if self.is_empty:
+            raise Exception("Can not pop, heap is empty")
+        else:
+            new_root = self._heap_arr.pop()
+            old_root = self._heap_arr[0]
+            if len(self._heap_arr) == 0:
+                self.is_empty = True
+            else:
+                self._heap_arr[0] = new_root
+                self._sift_down()
+            return old_root
+
--- data_structures/test_heap.py
+++ data_structures/test_heap.py
@@ -5,52 +5,40 @@ from heap_type import HeapType
 
 class TestHeap(unittest.TestCase):
     def setUp(self):
-        self.heap = Heap(kind=HeapType.MAX)
-        self.test_val_1 = 10
-        self.test_vals_1 = [5, 7, 4, 10, 8, 6]
-        self.test_vals_1_max_heap = [10, 8, 6, 5, 7, 4]
-        self.test_vals_1_min_heap = [4, 7, 5, 10, 8, 6]
-        self.test_vals_2 = [1, 1, 2, 1, 1]
-        self.test_vals_2_max_heap = [2, 1, 1, 1, 1]
-        self.test_vals_2_min_heap = [1, 1, 2, 1, 1]
+        self.heap = Heap()
+        self.val_1 = 10
+        self.inputs = [[5, 7, 4, 10, 8, 6], [1, 1, 2, 1, 1]]
+        self.inputs_solutions_max = [[10, 8, 6, 5, 7, 4], [2, 1, 1, 1, 1]]
+        self.inputs_solutions_min = [[4, 7, 5, 10, 8, 6], [1, 1, 2, 1, 1]]
 
     def test_push(self):
-        for val in self.test_vals_1:
-            self.heap.push(val)
-        self.assertEqual(self.heap._heap_arr, self.test_vals_1_max_heap)
-        self.assertEqual(self.heap.size, len(self.test_vals_1))
-        self.assertEqual(self.heap.is_empty, False)
-
-        self.heap.reset()
-        for val in self.test_vals_2:
-            self.heap.push(val)
-        self.assertEqual(self.heap._heap_arr, self.test_vals_2_max_heap)
-        self.assertEqual(self.heap.size, len(self.test_vals_2))
-        self.assertEqual(self.heap.is_empty, False)
-
-    def test_push_min(self):
-        self.heap = Heap(HeapType.MIN)
-        for val in self.test_vals_1:
-            self.heap.push(val)
-        self.assertEqual(self.heap._heap_arr, self.test_vals_1_min_heap)
-        self.assertEqual(self.heap.size, len(self.test_vals_1))
-        self.assertEqual(self.heap.is_empty, False)
-
-        self.heap.reset()
-        for val in self.test_vals_2:
-            self.heap.push(val)
-        self.assertEqual(self.heap._heap_arr, self.test_vals_2_min_heap)
-        self.assertEqual(self.heap.size, len(self.test_vals_2))
-        self.assertEqual(self.heap.is_empty, False)
+        for heap_type in HeapType:
+            self.heap.kind = heap_type
+            for i in range(len(self.inputs)):
+                for val in self.inputs[i]:
+                    self.heap.push(val)
+                if self.heap.kind == HeapType.MAX:
+                    self.assertEqual(self.heap._heap_arr, self.inputs_solutions_max[i])
+                else:
+                    self.assertEqual(self.heap._heap_arr, self.inputs_solutions_min[i])
+                self.assertEqual(self.heap.size, len(self.inputs[i]))
+                self.assertEqual(self.heap.is_empty, False)
+                self.heap.empty()
 
     @unittest.expectedFailure
     def test_peek_empty(self):
-        self.heap.peek()
+        for heap_type in HeapType:
+            self.heap.kind = heap_type
+            self.heap.peek()
+            self.heap.empty()
 
     def test_peek(self):
-        self.heap.push(self.test_val_1)
-        root = self.heap.peek()
-        self.assertEqual(root, self.test_val_1)
+        for heap_type in HeapType:
+            self.heap.kind = heap_type
+            self.heap.push(self.val_1)
+            root = self.heap.peek()
+            self.assertEqual(root, self.val_1)
+            self.heap.empty()
 
 
 if __name__ == "__main__":
