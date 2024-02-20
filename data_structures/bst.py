8852be28dda1fc4697c3d29c1f0a95e3f091539d
DylanPJackson <frprdjackson@gmail.com>
2024-02-20 13:06:15 -0800

    Updated search, added traverse and insert functionality. Updated tests.

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -29,7 +29,17 @@ class BST:
         else:
             return False
 
+    def empty(self):
+        self.neighbors = {}
+        self.root = None
+
     def search(self, search_key: int):
+        """
+        Return search_key if exists, otherwise none
+
+        :param search_key: key to search for
+        :return: int, None
+        """
         root_key = self.root
         while root_key is not None:
             if root_key == search_key:
@@ -41,7 +51,7 @@ class BST:
             else:
                 raise Exception("Issue searching with search_key: {search_key} and root_key: {root_key}"
                                 .format(search_key=search_key, root_key=root_key))
-        return None
+        return root_key
 
     def _get_child(self, parent: int, side: str):
         """
@@ -57,6 +67,77 @@ class BST:
             elif side == "right":
                 return nbr_list[1]
             else:
-                raise Exception("Side {side} not supported".format(side=side))
+                raise Exception("Side : {side} not supported".format(side=side))
         else:
-            raise Exception("Parent key : {parent} does not exist in tree".format(parent=parent))
+            raise Exception("Key : {parent} does not exist in tree".format(parent=parent))
+
+    def insert(self, ins_key: int):
+        """
+        Add key to BST
+
+        :param ins_key: key to insert
+        :return: None
+        """
+        if self.is_empty:
+            self.root = ins_key
+            self.neighbors[ins_key] = [None, None]
+        else:
+            curr_key = self.root
+            while curr_key is not None:
+                if ins_key <= curr_key:
+                    if self.neighbors[curr_key][0] is None:
+                        self.neighbors[curr_key][0] = ins_key
+                        self.neighbors[ins_key] = [None, None]
+                        break
+                    else:
+                        curr_key = self.neighbors[curr_key][0]
+                else:
+                    if self.neighbors[curr_key][1] is None:
+                        self.neighbors[curr_key][1] = ins_key
+                        self.neighbors[ins_key] = [None, None]
+                        break
+                    else:
+                        curr_key = self.neighbors[curr_key][1]
+
+    def _rec_traverse(self, order: str, curr_key: int):
+        """
+        Recursively traverse tree to return string representation of tree with given order
+
+        :param order:
+        :param curr_key:
+        :return: string representation of tree
+        """
+        if curr_key is None:
+            return ""
+        else:
+            if order == 'pre':
+                return (' {curr_key} '.format(curr_key=curr_key) +
+                        self._rec_traverse(order, self.neighbors[curr_key][0]) +
+                        self._rec_traverse(order, self.neighbors[curr_key][1]))
+            elif order == 'post':
+                return (self._rec_traverse(order, self.neighbors[curr_key][0]) +
+                        self._rec_traverse(order, self.neighbors[curr_key][1]) +
+                        ' {curr_key} '.format(curr_key=curr_key))
+            elif order == 'in':
+                return (self._rec_traverse(order, self.neighbors[curr_key][0]) +
+                        ' {curr_key} '.format(curr_key=curr_key) +
+                        self._rec_traverse(order, self.neighbors[curr_key][1]))
+            else:
+                raise Exception("Can not perform traverse with order : {order}".format(order=order))
+
+    def traverse(self, order: str):
+        """
+        Return a string representation of the tree in the given traverse order
+        :param order: str, [pre, post, in]
+        :return: string representation of tree
+        """
+        curr_key = self.root
+        return self._rec_traverse(order, curr_key)
+
+    def delete(self, key: int):
+        """
+        Remove given key from tree
+        :param key: key to remove
+        :return: None
+        """
+        pass
--- data_structures/test_bst.py
+++ data_structures/test_bst.py
@@ -5,12 +5,59 @@ from bst import BST
 class TestBST(unittest.TestCase):
     def setUp(self):
         self.bst = BST()
+        self.arr_1 = [8, 11, 4, 12, 7, 1, 10]
+        self.arr_1_in = ' 1  4  7  8  10  11  12 '
+        self.arr_1_pre = ' 8  4  1  7  11  10  12 '
+        self.arr_1_post = ' 1  7  4  10  12  11  8 '
+        self.arr_2 = [12, 6, 18, 9, 14]
+        self.arr_2_in = ' 6  9  12  14  18 '
 
-    def test_search(self):
-        # Test empty search
+    def test_insert(self):
+        for key in self.arr_1:
+            self.bst.insert(key)
+        trav_str = self.bst.traverse('in')
+        self.assertEqual(trav_str, self.arr_1_in)
+
+        self.bst.empty()
+
+        for key in self.arr_2:
+            self.bst.insert(key)
+        trav_str = self.bst.traverse('in')
+        self.assertEqual(trav_str, self.arr_2_in)
+
+    def test_traverse(self):
+        for key in self.arr_1:
+            self.bst.insert(key)
+        in_str = self.bst.traverse('in')
+        self.assertEqual(in_str, self.arr_1_in)
+        pre_str = self.bst.traverse('pre')
+        self.assertEqual(pre_str, self.arr_1_pre)
+        post_str = self.bst.traverse('post')
+        self.assertEqual(post_str, self.arr_1_post)
+
+    def test_search_empty(self):
         search_key = 10
         search_result = self.bst.search(search_key)
         self.assertEqual(search_result, None)
+
+    def test_search_missing(self):
+        for key in self.arr_1:
+            self.bst.insert(key)
+
+        search_key = 13
+        search_result = self.bst.search(search_key)
+        self.assertEqual(search_result, None)
+
+        search_key = 0
+        search_result = self.bst.search(search_key)
+        self.assertEqual(search_result, None)
+
+    def test_search(self):
+        for key in self.arr_1:
+            self.bst.insert(key)
+        for search_key in self.arr_1:
+            search_result = self.bst.search(search_key)
+            self.assertEqual(search_result, search_key)
 
 
 if __name__ == "__main__":
