ecacefea1a0206790e95daeec81584ddf5f17e9b
DylanPJackson <frprdjackson@gmail.com>
2024-02-21 21:02:45 -0800

    Updated get_child to get_nbr, started delete implementation.

--- data_structures/bst.py
+++ data_structures/bst.py
@@ -19,12 +19,12 @@ class BST:
     traverse (internal) : traverse tree in given order
     """
     def __init__(self):
-        self.neighbors = {}  # {'par_key': [left_child, right_child], ...}
+        self.neighbors = {}  # {'par_key': [parent, left_child, right_child], ...}
         self.root = None
 
     @property
     def is_empty(self):
-        if self.root is None:
+        if self.root is None and self.neighbors == {}:
             return True
         else:
             return False
@@ -33,71 +33,30 @@ class BST:
         self.neighbors = {}
         self.root = None
 
-    def search(self, search_key: int):
+    def _get_nbr(self, key: int, rel: str, side: str = None):
         """
-        Return search_key if exists, otherwise none
-
-        :param search_key: key to search for
-        :return: int, None
-        """
-        root_key = self.root
-        while root_key is not None:
-            if root_key == search_key:
-                return root_key
-            elif search_key < root_key:
-                root_key = self._get_child(root_key, 'left')
-            elif search_key > root_key:
-                root_key = self._get_child(root_key, 'right')
-            else:
-                raise Exception("Issue searching with search_key: {search_key} and root_key: {root_key}"
-                                .format(search_key=search_key, root_key=root_key))
-        return root_key
-
-    def _get_child(self, parent: int, side: str):
-        """
-        Get the child from given side from given parent
-        :param parent: int
+        Get the node with given relation
+        :param key: int
         :param side: 'left' or 'right'
-        :return: int if child exist, None otherwise
+        :param rel: Relation to key, either 'parent' or 'child'
+        :return: int if relative exists, None otherwise
         """
-        if parent in self.neighbors:
-            nbr_list = self.neighbors[parent]
-            if side == "left":
-                return nbr_list[0]
-            elif side == "right":
-                return nbr_list[1]
-            else:
-                raise Exception("Side : {side} not supported".format(side=side))
-        else:
-            raise Exception("Key : {parent} does not exist in tree".format(parent=parent))
-
-    def insert(self, ins_key: int):
-        """
-        Add key to BST
-
-        :param ins_key: key to insert
-        :return: None
-        """
-        if self.is_empty:
-            self.root = ins_key
-            self.neighbors[ins_key] = [None, None]
-        else:
-            curr_key = self.root
-            while curr_key is not None:
-                if ins_key <= curr_key:
-                    if self.neighbors[curr_key][0] is None:
-                        self.neighbors[curr_key][0] = ins_key
-                        self.neighbors[ins_key] = [None, None]
-                        break
-                    else:
-                        curr_key = self.neighbors[curr_key][0]
+        if key in self.neighbors:
+            nbr_list = self.neighbors[key]
+            if rel == 'child':
+                if side == "left":
+                    return nbr_list[1]
+                elif side == "right":
+                    return nbr_list[2]
                 else:
-                    if self.neighbors[curr_key][1] is None:
-                        self.neighbors[curr_key][1] = ins_key
-                        self.neighbors[ins_key] = [None, None]
-                        break
-                    else:
-                        curr_key = self.neighbors[curr_key][1]
+                    raise Exception("Side : {side} not supported".format(side=side))
+            elif rel == 'parent':
+                return nbr_list[0]
+            else:
+                raise Exception("Relation: {rel} not supported".format(rel=rel))
+
+        else:
+            raise Exception("Key : {key} does not exist in tree".format(key=key))
 
     def _rec_traverse(self, order: str, curr_key: int):
         """
@@ -112,18 +71,66 @@ class BST:
         else:
             if order == 'pre':
                 return (' {curr_key} '.format(curr_key=curr_key) +
-                        self._rec_traverse(order, self.neighbors[curr_key][0]) +
-                        self._rec_traverse(order, self.neighbors[curr_key][1]))
-            elif order == 'post':
-                return (self._rec_traverse(order, self.neighbors[curr_key][0]) +
                         self._rec_traverse(order, self.neighbors[curr_key][1]) +
+                        self._rec_traverse(order, self.neighbors[curr_key][2]))
+            elif order == 'post':
+                return (self._rec_traverse(order, self.neighbors[curr_key][1]) +
+                        self._rec_traverse(order, self.neighbors[curr_key][2]) +
                         ' {curr_key} '.format(curr_key=curr_key))
             elif order == 'in':
-                return (self._rec_traverse(order, self.neighbors[curr_key][0]) +
+                return (self._rec_traverse(order, self.neighbors[curr_key][1]) +
                         ' {curr_key} '.format(curr_key=curr_key) +
-                        self._rec_traverse(order, self.neighbors[curr_key][1]))
+                        self._rec_traverse(order, self.neighbors[curr_key][2]))
             else:
                 raise Exception("Can not perform traverse with order : {order}".format(order=order))
+
+    def search(self, search_key: int):
+        """
+        Return search_key if exists, otherwise none
+
+        :param search_key: key to search for
+        :return: int, None
+        """
+        root_key = self.root
+        while root_key is not None:
+            if root_key == search_key:
+                return root_key
+            elif search_key < root_key:
+                root_key = self._get_nbr(root_key, 'child', 'left')
+            elif search_key > root_key:
+                root_key = self._get_nbr(root_key, 'child', 'right')
+            else:
+                raise Exception("Issue searching with search_key: {search_key} and root_key: {root_key}"
+                                .format(search_key=search_key, root_key=root_key))
+        return root_key
+
+    def insert(self, ins_key: int):
+        """
+        Add key to BST
+
+        :param ins_key: key to insert
+        :return: None
+        """
+        if self.is_empty:
+            self.root = ins_key
+            self.neighbors[ins_key] = [None, None, None]
+        else:
+            curr_key = self.root
+            while curr_key is not None:
+                if ins_key <= curr_key:
+                    if self.neighbors[curr_key][1] is None:
+                        self.neighbors[curr_key][1] = ins_key
+                        self.neighbors[ins_key] = [curr_key, None, None]
+                        break
+                    else:
+                        curr_key = self.neighbors[curr_key][1]
+                else:
+                    if self.neighbors[curr_key][2] is None:
+                        self.neighbors[curr_key][2] = ins_key
+                        self.neighbors[ins_key] = [curr_key, None, None]
+                        break
+                    else:
+                        curr_key = self.neighbors[curr_key][2]
 
     def traverse(self, order: str):
         """
@@ -136,8 +143,22 @@ class BST:
 
     def delete(self, key: int):
         """
-        Remove given key from tree
+        Remove given key from tree. Replace with in-order successor.
         :param key: key to remove
         :return: None
         """
-        pass
+        # Case 1 : No children
+        if self.is_empty:
+            raise Exception("Cannot delete from empty tree")
+        else:
+            if key in self.neighbors:
+                left_c = self._get_nbr(key, 'child', 'left')
+                right_c = self._get_nbr(key, 'child', 'right')
+                parent = self._get_nbr(key, 'parent')
+                if left_c is None and right_c is None:
+                    if parent is not None:
+                        pass
+            else:
+                raise Exception("Cannot delete key: {key} as it does not exist in bst")
+        # Case 2 : One child
+        # Case 3 : Two children
