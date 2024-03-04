e67f142963712ea09bc208bc45e2e54468600579
DylanPJackson <frprdjackson@gmail.com>
2024-03-04 12:05:19 -0800

    Added access and exists methods, as well as HashKeyPair class.

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -16,21 +16,82 @@ class HashTable:
     def __str__(self):
         string_rep = ""
         for ind in range(self.capacity):
-            add_string = "{} : {}\n".format(ind, self.table[0])
+            add_string = "{} : {}\n".format(ind, str(self.table[ind]))
             string_rep += add_string
         return string_rep
 
-    def insert(self, val: Any):
+    def _get_hash_index(self, key: Any):
+        hash_val = hash(key)  # OK with possible TypeError here because we can't use a non-hashable object anyway
+        index = int((hash_val % self.capacity) // 1)
+        return index
+
+    def _traverse_ll_for_key(self, ll: LinkedList, key: Any):
         """
-        Insert val into hash table
+        Traverse linked list in search of key
+        :param ll:
+        :param key:
+        :return: HashKeyPair, None
+        """
+        for ll_ind in range(ll.size):
+            node = ll.access_ind(ll_ind)
+            if isinstance(node, HashKeyPair):
+                if node.key == key:
+                    return node
+            else:
+                raise Exception("Item in linked list is not HashKeyPair. Please ensure all items added to the"
+                                "HashTable are converted to HashKeyPair. Item : {}".format(node))
+        return None
+
+    def empty(self):
+        self.size = 0
+        self.capacity = 10
+        self.table = [LinkedList() for i in range(self.capacity)]
+
+    def insert(self, key: Any, val: Any):
+        """
+        Insert key with val into hash table
+        :param key: Any
         :param val: Any
         :return: None
         """
-        try:
-            hash_val = hash(val)
-        except TypeError:
-            print("Can not hash {} with type {}".format(val, type(val)))
-            return
-        index = int((hash_val % self.capacity) // 1)
-        self.table[index].insert(val)
+        index = self._get_hash_index(key)
+        keypair = HashKeyPair(key, val)
+        self.table[index].insert(keypair)
         self.size += 1
+
+    def exists(self, key: Any):
+        """
+        Returns boolean of if key exists in hash table or not
+        :param key: Any
+        :return: boolean
+        """
+        for table_ind in range(self.capacity):
+            ll = self.table[table_ind]
+            node = self._traverse_ll_for_key(ll, key)
+            if node is not None:
+                return True
+        return False
+
+    def access(self, key: Any):
+        """
+        Return value of key in table if it exists
+        :param key: any
+        :return: value of key, Any
+        """
+        if self.exists(key):
+            index = self._get_hash_index(key)
+            ll = self.table[index]
+            node = self._traverse_ll_for_key(ll, key)
+            if node is not None:
+                return node.val
+        else:
+            raise Exception("Key : {} does not exist in table".format(key))
+
+
+class HashKeyPair:
+    def __init__(self, key: Any, val: Any):
+        self.key = key
+        self.val = val
+
+    def __repr__(self):
+        return "HashKeyPair(key: {}, val: {})".format(self.key, self.val)
--- data_structures/test_hash_table.py
+++ data_structures/test_hash_table.py
@@ -5,9 +5,43 @@ from hash_table import HashTable
 class TestHashtable(unittest.TestCase):
     def setUp(self):
         self.hash_table = HashTable()
+        self.key_1 = 'dogs'
+        self.key_2 = 'cats'
+        self.val_1 = 1
+        self.val_2 = 0
+
+    def tearDown(self):
+        self.hash_table.empty()
 
     def test_insert(self):
+        self.hash_table.insert(self.key_1, self.val_1)
+        self.hash_table.insert(self.key_2, self.val_2)
         print(self.hash_table)
+
+    @unittest.expectedFailure
+    def test_access_no_exist(self):
+        self.hash_table.access('aquarius')
+
+    def test_access(self):
+        self.hash_table.insert(self.key_1, self.val_1)
+        val_1 = self.hash_table.access(self.key_1)
+        self.assertEqual(self.val_1, val_1)
+
+        self.hash_table.insert(self.key_2, self.val_2)
+        val_2 = self.hash_table.access(self.key_2)
+        self.assertEqual(self.val_2, val_2)
+
+    def test_exists(self):
+        exist = self.hash_table.exists('aquarius')
+        self.assertFalse(exist)
+
+        self.hash_table.insert(self.key_1, self.val_1)
+        exist = self.hash_table.exists(self.key_1)
+        self.assertTrue(exist)
+
+        self.hash_table.insert(self.key_2, self.val_2)
+        exist = self.hash_table.exists(self.key_2)
+        self.assertTrue(exist)
 
 
 if __name__ == "__main__":
